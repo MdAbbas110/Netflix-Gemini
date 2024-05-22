beb1a8ee08568f596fd9b1b607380b34be726eab
Mohammad Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2024-05-22 23:56:48 +0530

    Create explore.js
    added random funtion

---
+++ public/explore.js
@@ -0,0 +1,8 @@
+// Here the files that will be store needs to be added 
+
+export function explore () {
+    const init = Math.random(Math.floor() * 110)
+  return init
+}
+
+let global = [12,14,72,110]
